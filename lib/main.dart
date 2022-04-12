d757a9ad92dc7d3a10eca0ef64e13409f23c8f16
aqaz99 <jminerswebb@gmail.com>
2022-04-11 17:00:43 -0700

    Better home screen

--- lib/main.dart
+++ lib/main.dart
@@ -54,21 +54,34 @@ class _MyHomePageState extends State<MyHomePage> {
                   // The long text inside this column overflows. Remove the row and column above this comment and the text wraps.
                   Expanded(
                     child: Column(
-                      children: const <Widget>[
-                        Text(
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(6.0),
+                            ),
+                          ],
+                        ),
+                        const Text(
                           "Askcent",
                           style: TextStyle(
                               // backgroundColor: Colors.white,
-                              fontSize: 24,
+                              fontSize: 42,
                               fontWeight: FontWeight.bold,
                               color: Colors.black),
                         ),
-                        Text(
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                            ),
+                          ],
+                        ),
+                        const Text(
                           "Test your skills at recognizing accents!",
                           style: TextStyle(
                               // backgroundColor: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 32,
                               color: Colors.black),
                           textAlign: TextAlign.center,
                         )
@@ -77,11 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ],
               ),
-
               Row(
                 children: [
                   Container(
-                    padding: const EdgeInsets.all(120.0),
+                    padding: const EdgeInsets.all(100.0),
                   ),
                 ],
               ),
@@ -93,23 +105,32 @@ class _MyHomePageState extends State<MyHomePage> {
                   const Icon(
                     Icons.record_voice_over,
                     color: Colors.greenAccent,
-                    size: 24.0,
+                    size: 34.0,
                     semanticLabel: 'Text to announce in accessibility modes',
                   ),
-                  TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromARGB(209, 10, 100, 48)),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
+                  // Button example used as a template from here:
+                  // https://stackoverflow.com/questions/54828454/how-to-set-rounded-border-to-a-materialbutton-on-flutter
+                  Material(
+                    //Wrap with Material
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22.0)),
+                    elevation: 18.0,
+                    color: Color.fromARGB(255, 18, 165, 38),
+                    clipBehavior: Clip.antiAliasWithSaveLayer, // Add This
+                    child: MaterialButton(
+                      minWidth: 240.0,
+                      height: 50,
+                      color: Color.fromARGB(255, 21, 185, 43),
+                      child: const Text('Play Askcent!',
+                          style:
+                              TextStyle(fontSize: 20.0, color: Colors.white)),
+                      onPressed: () {},
                     ),
-                    onPressed: () {},
-                    child: const Text('Play Askcent!'),
                   ),
                   const Icon(
                     Icons.location_searching_sharp,
                     color: Colors.greenAccent,
-                    size: 36.0,
+                    size: 34.0,
                   ),
                 ],
               ),
