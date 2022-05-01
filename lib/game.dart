893905f18a2e54fb4ea0fba540e7ddf1a8f40633
aqaz99 <jminerswebb@gmail.com>
2022-05-01 12:47:17 -0700

    Sound working, draggable markers, leaderboard screen pretty good

--- lib/game.dart
+++ lib/game.dart
@@ -5,52 +5,10 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // Play sound
-import 'package:flutter_sound/flutter_sound.dart';
-
-/// Determine the current position of the device.
-///
-/// When the location services are not enabled or permissions
-/// are denied the `Future` will return an error.
-Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
-
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  return await Geolocator.getCurrentPosition();
-}
+import 'package:audioplayers/audioplayers.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -75,6 +33,8 @@ class GameScreenState extends State<MapSample> {
   final Completer<GoogleMapController> _controller = Completer();
   List<Marker> _currentMarkers = [];
   late Marker _markerGuess;
+  final player = new AudioCache(fixedPlayer: AudioPlayer());
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -84,16 +44,8 @@ class GameScreenState extends State<MapSample> {
   String myLocation = "";
   @override
   void initState() {
-    FlutterSound flutterSound = new FlutterSound();
+    // FlutterSound flutterSound = new FlutterSound();
     super.initState();
-    _currentMarkers.add(_markerGuess = Marker(
-        markerId: const MarkerId("Marker 1"),
-        draggable: false,
-        onTap: () {
-          print("Tapped me");
-        },
-        infoWindow: InfoWindow(title: 'The title of the marker'),
-        position: const LatLng(37.42796133580664, -122.085749655962)));
   }
 
   @override
@@ -107,6 +59,7 @@ class GameScreenState extends State<MapSample> {
         iconTheme: const IconThemeData(color: Colors.black),
         actions: [
           TextButton(
+            // Submit guess button
             style: TextButton.styleFrom(
               padding: const EdgeInsets.all(16.0),
               primary: Colors.white,
@@ -115,7 +68,7 @@ class GameScreenState extends State<MapSample> {
             onPressed: () {
               print("Hello from gradient");
             },
-            child: const Text('Gradient'),
+            child: const Text('Submit Guess!'),
           ),
         ],
       ),
@@ -134,24 +87,17 @@ class GameScreenState extends State<MapSample> {
             });
           }),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToCurrentPos,
-        label: const Text('To Current Location!'),
-        icon: const Icon(Icons.home),
+        onPressed: _playVoice,
+        label: const Text('Play Voice Clip'),
+        icon: const Icon(Icons.volume_up_outlined),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
     );
   }
 
-  Future<void> _goToCurrentPos() async {
-    Position p = await _determinePosition();
-    CameraPosition _userPosition = CameraPosition(
-        bearing: 192.8334901395799,
-        target: LatLng(p.latitude, p.longitude),
-        tilt: 30,
-        zoom: 19);
-
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_userPosition));
+  Future<void> _playVoice() async {
+    player.fixedPlayer!.stop();
+    player.play('sounds/tts_output.wav');
   }
 
   void _addMarker(LatLng position) {
@@ -159,7 +105,7 @@ class GameScreenState extends State<MapSample> {
     setState(() {
       _currentMarkers.add(Marker(
           markerId: const MarkerId("My Guess"),
-          draggable: false,
+          draggable: true,
           visible: true,
           onTap: () {
             print("Tapped me");
--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -3,26 +3,73 @@ import 'package:askcent/drawer.dart';
 
 class LeaderboardScreen extends StatelessWidget {
   const LeaderboardScreen({Key? key}) : super(key: key);
-
+  static List<String> list_of_users = [
+    "Tom",
+    "Bob",
+    "Jess",
+    "Earl",
+    "Pat",
+    "Cat",
+    "Marge",
+    "Harry",
+    "Patrick",
+    "Jane",
+    "Teresa"
+  ];
+  static List<String> list_of_items = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text(
-          "Global Leaderboard",
-          style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text(
+            "Global Leaderboard",
+            style: TextStyle(color: Colors.black),
+          ),
+          iconTheme: const IconThemeData(color: Colors.black),
         ),
-        iconTheme: const IconThemeData(color: Colors.black),
-      ),
-      drawer: const AskcentDrawer(),
-      body: const Center(
-        child: Text(
-          'Global Leaderboards',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
-    );
+        drawer: const AskcentDrawer(),
+        body: ListView.builder(
+            itemCount: list_of_items.length,
+            itemBuilder: (BuildContext ctxt, int index) {
+              return Container(
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(6.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.black)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Icon(
+                      Icons.account_circle_outlined,
+                      color: Colors.greenAccent,
+                      size: 34.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    Text(list_of_users[index]),
+                    const Icon(
+                      Icons.emoji_events_rounded,
+                      color: Colors.greenAccent,
+                      size: 34.0,
+                    ),
+                    Text(list_of_items[index]),
+                  ],
+                ),
+              );
+            }));
   }
 }
